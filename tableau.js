c4cb48d56ca5135e30a5764fdf4a898cc0467048
nguyenndt278 <53121073+nguyenndt278@users.noreply.github.com>
2020-01-13 19:20:00 -0600

    01/13/20

--- tableau.js
+++ tableau.js
@@ -2,7 +2,7 @@ function initializeViz() {
     // JS object that points at empty div in the html
     var placeholderDiv = document.getElementById("tableauViz");
     // URL of the viz to be embedded
-    var url = "https://prod-useast-a.online.tableau.com/t/nguyennguyen/views/NY_Biker/BikerStory?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link"
+    var url = "https://prod-useast-a.online.tableau.com/t/nguyendtnguyen/views/NY_City_Bike/BikerStory?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link"
     // An object that contains options specifying how to embed the viz
     var options = {
       width: '600px',
